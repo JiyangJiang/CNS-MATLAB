e28e7a83144dec5dd3978f43c4ce97829839002b
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2020-06-21 22:45:03 +1000

    Update cns2_wmh_ud_initDirFile.m

--- wmh/cns2_wmh_ud_initDirFile.m
+++ wmh/cns2_wmh_ud_initDirFile.m
@@ -1,10 +1,12 @@
 
-parfor (i = 1 : cns2param.n_subjs, cns2param.n_cpus)
-	mkdir (cns2param.subjs_dir, cns2param.subjs_list{i,1});
+parfor (i = 1 : cns2param.n_subjs, cns2param.exe.n_cpus)
 
-	copyfile (fullfile (cns2param.study_dir, 'T1', cns2param.t1_list{i,1}), ...
-				fullfile(cns2param.subjs_dir, cns2param.subjs_list{i,1}, 't1.nii'));
+	mkdir (cns2param.dirs.subjs, cns2param.lists.subjs{i,1});
 
-	copyfile (fullfile (cns2param.study_dir, 'FLAIR', cns2param.flair_list{i,1}), ...
-				fullfile(cns2param.subjs_dir, cns2param.subjs_list{i,1}, 'flair.nii'));
+	copyfile (fullfile (cns2param.dirs.study, 'T1', cns2param.lists.t1{i,1}), ...
+			  fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 't1.nii'));
+
+	copyfile (fullfile (cns2param.dirs.study, 'FLAIR', cns2param.lists.flair{i,1}), ...
+			  fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 'flair.nii'));
+	
 end
