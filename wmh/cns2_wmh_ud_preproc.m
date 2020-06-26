76e9b5324954efbd4efb522c6bd5ffd154d3c781
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2020-06-26 14:49:56 +1000

    Update cns2_wmh_ud_preproc.m

--- wmh/cns2_wmh_ud_preproc.m
+++ wmh/cns2_wmh_ud_preproc.m
@@ -15,8 +15,10 @@ for i = 1 : cns2param.n_subjs
 	flair = fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 'flair.nii');
 
 	try
-		% coregistration
-		rflair = cns2_spmbatch_coregistration (cns2param, flair, t1, 'same_dir');
+		if cns2param.classification.ud.ext_space == 'dartel'
+			% coregistration
+			rflair = cns2_spmbatch_coregistration (cns2param, flair, t1, 'same_dir');
+		end
 
 		% t1 segmentation
 		[cGM,cWM,cCSF,rcGM,rcWM,rcCSF] = cns2_spmbatch_segmentation (cns2param, t1);
@@ -31,22 +33,31 @@ for i = 1 : cns2param.n_subjs
 											cns2param.templates.temp1_6{5,1}, ...
 											cns2param.templates.temp1_6{6,1});
 
-		% bring t1, flair, gm, wm, csf to DARTEL space (create warped)
-		wt1     = cns2_spmbatch_nativeToDARTEL (cns2param, t1,     flowmap);
-		wrflair = cns2_spmbatch_nativeToDARTEL (cns2param, rflair, flowmap);
-		wcGM    = cns2_spmbatch_nativeToDARTEL (cns2param, cGM,    flowmap);
-		wcWM    = cns2_spmbatch_nativeToDARTEL (cns2param, cWM,    flowmap);
-		wcCSF   = cns2_spmbatch_nativeToDARTEL (cns2param, cCSF,   flowmap);
+		switch cns2param.classification.ud.ext_space
+		case 'dartel'
+			% bring t1, flair, gm, wm, csf to DARTEL space (create warped)
+			wt1     = cns2_spmbatch_nativeToDARTEL (cns2param, t1,     flowmap);
+			wrflair = cns2_spmbatch_nativeToDARTEL (cns2param, rflair, flowmap);
+			wcGM    = cns2_spmbatch_nativeToDARTEL (cns2param, cGM,    flowmap);
+			wcWM    = cns2_spmbatch_nativeToDARTEL (cns2param, cWM,    flowmap);
+			wcCSF   = cns2_spmbatch_nativeToDARTEL (cns2param, cCSF,   flowmap);
 
-		% mask wrflair and wt1
-		cns2_scripts_mask  (cns2param, ...
-							wt1, ...
-							cns2param.templates.brnmsk, ...
-							fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 'wt1_brn.nii'));
-		cns2_scripts_mask  (cns2param, ...
-							wrflair, ...
-							cns2param.templates.brnmsk, ...
-							fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 'wrflair_brn.nii'));
+			% mask wrflair and wt1
+			cns2_scripts_mask  (cns2param, ...
+								wt1, ...
+								cns2param.templates.brnmsk, ...
+								fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 'wt1_brn.nii'));
+			cns2_scripts_mask  (cns2param, ...
+								wrflair, ...
+								cns2param.templates.brnmsk, ...
+								fullfile (cns2param.dirs.subjs, cns2param.lists.subjs{i,1}, 'wrflair_brn.nii'));
+		case 'native'
+			% preprocessing if extracting in native space
+			% - reverse flowmap
+			% - reverse templates to native
+			% - reverse brain mask to native
+			% - mask native flair and t1
+		end
 	
 		fprintf ('%s : %s finished preprocessing without errors.\n', curr_cmd, cns2param.lists.subjs{i,1});
 
