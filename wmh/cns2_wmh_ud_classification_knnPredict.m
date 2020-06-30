69976743e60da9f702bbcc80a269f399f80d1919
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2020-06-30 17:18:11 +1000

    Update cns2_wmh_ud_classification_knnPredict.m

--- wmh/cns2_wmh_ud_classification_knnPredict.m
+++ wmh/cns2_wmh_ud_classification_knnPredict.m
@@ -1,4 +1,4 @@
-function cns2_wmh_ud_classification_knnPredict (cns2param,f_tbl)
+function cns2_wmh_ud_classification_knnPredict (cns2param,f_tbl,lv2clstrs_struct)
 
 knn_mdl = loadLearnerForCoder(fullfile (cns2param.dirs.cns2, 'wmh', 'cns2_wmh_ud_classification_knnMdl.mat'));
 
@@ -6,4 +6,13 @@ knn_mdl = loadLearnerForCoder(fullfile (cns2param.dirs.cns2, 'wmh', 'cns2_wmh_ud
 % cluster index, and 12:14 columns are centroid coordinates
 [label,score,cost] = predict (knn_mdl, f_tbl(:,3:11));
 
-% --== TBC ==--
+% add the probability to the last column of feature table
+fd_tbl = [f_tbl score];??????
+
+% assign label to 2nd-level clusters
+switch cns2param.classification.ud.lv1clstr_method
+case 'kmeans'
+	Nlv1clstrs = cns2param.classification.ud.k4kmeans;
+case 'superpixel'
+	Nlv1clstrs = cns2param.classification.ud.n4superpixel_actual;
+end
